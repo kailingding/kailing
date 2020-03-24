8130d15db24399953842efd8f72049524331b921
Kailing Ding <markdingkl@gmail.com>
2020-03-24 00:16:02 -0700

    testing new deployment method

---
+++ build/precache-manifest.0c8cccb85ed25257d7f9ccb4572cc529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a641ca6ec6916e620764279ca68d2d0f",
+    "url": "/kailing/index.html"
+  },
+  {
+    "revision": "d5adb836b2ee1220bbe3",
+    "url": "/kailing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/kailing/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kailing/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5adb836b2ee1220bbe3",
+    "url": "/kailing/static/js/main.10a24b1a.chunk.js"
+  },
+  {
+    "revision": "879a7c0a3c70f3e9f22f",
+    "url": "/kailing/static/js/runtime-main.7c939da0.js"
+  }
+]);

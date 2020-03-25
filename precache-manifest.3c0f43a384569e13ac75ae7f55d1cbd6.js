763d584977b1145bb2110475261be8a6e80e3d4b
Kailing Ding <markdingkl@gmail.com>
2020-03-25 00:08:36 -0700

    Updates

---
+++ precache-manifest.3c0f43a384569e13ac75ae7f55d1cbd6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1eca9806f41267b649b7709d4f1947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f751bdd96522172d3e",
+    "url": "/static/css/main.17b16078.chunk.css"
+  },
+  {
+    "revision": "e454ce2f0f94e96ced2c",
+    "url": "/static/js/2.caad343e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.caad343e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f751bdd96522172d3e",
+    "url": "/static/js/main.b8ed17ae.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "6c362e00c9a0cd2688c041229cb6446b",
+    "url": "/static/media/SourceSansPro-It.6c362e00.ttf"
+  },
+  {
+    "revision": "4e46b9edb8159b17fa61bc0a3d766f4a",
+    "url": "/static/media/me.4e46b9ed.jpg"
+  },
+  {
+    "revision": "1ff81849cdeaa0da2559a61412767e58",
+    "url": "/static/media/resume.1ff81849.pdf"
+  },
+  {
+    "revision": "785893bd94e5fb560e2ea0b664620e45",
+    "url": "/static/media/snowboard.785893bd.jpg"
+  }
+]);

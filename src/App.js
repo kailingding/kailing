4f93908ead4e94181d8935f2408abffcb997ef44
Kailing Ding <markdingkl@gmail.com>
2020-03-28 15:53:01 -0700

    app.js file minor change

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header/header.js";
 import AboutMe from "./components/aboutMe/aboutMe.js";
 import Title from "./components/sectionTitle/title.js";
 import Education from "./components/infoCard/education.js";
 import Experiences from "./components/infoCard/experiences.js";
+import Project from "./components/projects/project.js";
 import Adventures from "./components/adventures/adventures.js";
 
 function App() {
-  const github = require("./images/socialMedia/github.png");
-  const linkedin = require("./images/socialMedia/linkedin.png");
-  const wechat = require("./images/socialMedia/wechat.png");
-
   return (
     <div className="App">
-      <header className="home">
-        <div className="wrapper-content">
-          <h3>HELLO!</h3>
-          <h2>I'M KAILING DING</h2>
-          <span>who build data product with love</span>
-          <div></div>
-          <div className="hashtags">
-            <ul>
-              <li className="label-pill label-lightblue">#Entrepuership</li>
-              <li className="label-pill">#DataScience</li>
-              <li className="label-pill">#MachineLearning</li>
-              <li className="label-pill label-lightpink">#Adventures</li>
-            </ul>
-          </div>
-          <div className="social-media-link">
-            <ul>
-              <li>
-                <a href="https://www.linkedin.com/in/kailingding" target="_">
-                  <img src={linkedin} alt="linkedin" className="grayscale" />
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/kailingding" target="_">
-                  <img src={github} alt="github" className="grayscale"></img>
-                </a>
-              </li>
-              <li>
-                <img src={wechat} alt="linkedin" className="grayscale" />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </header>
+      <Header />
+
       {/* About me */}
       <div className="about">
         <section className="flex-container">
@@ -68,11 +35,12 @@ function App() {
         {/* Project Work */}
         <section className="project flex-container">
           <Title title="PROJECT WORK" name="project" />
+          <Project />
         </section>
 
         {/* Extreme Sports */}
         <section className="extreme-sports flex-container">
-          <Title title="Advantures" />
+          <Title title="Adventures" />
           <Adventures />
         </section>
       </div>
